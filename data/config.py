7acbe27af9d4d937fbf213eff43a87892e9a0c08
felixxsin <felix020@binus.ac.id>
2024-08-26 14:03:35 +0700

    Blum Script

---
+++ data/config.py
@@ -0,0 +1,47 @@
+# api id, hash
+API_ID = 25480165
+API_HASH = 'e45ad546fc4ca54999c717d0bd2e02c2'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '283993:kdmioieiweikiokeocki4okew' # API TOKEN get in @BotFather
+CHAT_ID = '22803822' # Your telegram id
+
+# Delay between connections to accounts
+ACC_DELAY = [5, 15]
+
+# Proxy type
+PROXY_TYPE = "socks5" # http/socks5
+
+# Directory for sessions (do not change)
+WORKDIR = "sessions/"
+
+# Use proxy
+USE_PROXY = True # True/False
+
+# Points from the game
+POINTS = [230, 500] #[min, max]
+
+# Spend diamonds
+SPEND_DIAMONDS = True # True/False
+
+# Sleep time between games (in seconds)
+SLEEP_GAME_TIME = [20,30] #[min,max]
+
+# Mini delays (in seconds)
+MINI_SLEEP = [2,6] #[min,max]
+
+# Additional delay after 8 hours
+SLEEP_8HOURS = [30*60,60*60] #[min,max] seconds
+# Range from 30 minutes (1800 seconds) to 60 minutes (3600 seconds)
+
+hello ='''  
+   
+███████╗███████╗██╗     ██╗██╗  ██╗███████╗ ██████╗ ███████╗████████╗
+██╔════╝██╔════╝██║     ██║╚██╗██╔╝██╔════╝██╔═══██╗██╔════╝╚══██╔══╝
+█████╗  █████╗  ██║     ██║ ╚███╔╝ ███████╗██║   ██║█████╗     ██║   
+██╔══╝  ██╔══╝  ██║     ██║ ██╔██╗ ╚════██║██║   ██║██╔══╝     ██║   
+██║     ███████╗███████╗██║██╔╝ ██╗███████║╚██████╔╝██║        ██║   
+╚═╝     ╚══════╝╚══════╝╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝   
+                                                                 
+'''
+
